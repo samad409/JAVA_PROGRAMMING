dc257b2d8ae42224784809d9be0cc007f51b219c
Abdul Samad <heyabdul409@gmail.com>
2025-04-05 12:49:58 +0530

    First Java Program

---
+++ MadLibGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MadLibGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a name : ");
+        String name = scanner.nextLine();
+        System.out.println("Enter a noun : ");
+        String noun = scanner.nextLine();
+        System.out.println("Enter a verb : ");
+        String verb = scanner.nextLine();
+        System.out.println("Enter a pronoun : ");
+        String pronoun = scanner.nextLine();
+        System.out.println("Enter a adjective : ");
+        String adjective = scanner.nextLine();
+
+        System.out.println("Name : " + name);
+        System.out.println("Noun : " + noun);
+        System.out.println("Verb : " + verb);
+        System.out.println("Pronoun : " + pronoun);
+        System.out.println("Adjective : " + adjective);
+
+        scanner.close();
+    }
+}
